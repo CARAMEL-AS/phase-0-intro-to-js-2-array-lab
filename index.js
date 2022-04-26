e0f842d34a256cd82528b1bd998b22d5cd468860
Aftab Sidhu <Aftab180@gmail.com>
2022-04-26 17:08:41 -0400

    commit tree b88661dd4833a89f3faa5636d6519d290543dccf

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+ console.log(cats);
+
+ function destructivelyAppendCat(ralph){
+    cats.push(ralph);
+}
+ 
+function destructivelyPrependCat(bob){
+    cats.unshift(bob);
+}
+
+function destructivelyRemoveLastCat(ralph){
+    cats.pop(ralph);
+}
+
+function destructivelyRemoveFirstCat(milo){
+    cats.shift(milo);
+}
+
+function appendCat(broom){
+    const Milo = [...cats,broom];
+    return Milo;
+}
+
+function prependCat(arnold){
+    const Otis = [arnold,...cats]
+    return Otis;
+}
+
+function removeLastCat(){
+    const lastCat = cats[cats.length - 1];
+    return cats.filter(cat => {return cat !== lastCat});
+}
+
+
+function removeFirstCat(){
+    return cats.filter(cat => {return cat !== cats[0]});
+}
+
+
+
